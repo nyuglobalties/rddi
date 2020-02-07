3c27422194075bb0487cbb58c7a6ea6b6b84019d
Patrick Anker <patricksanker@gmail.com>
2020-02-07 11:40:44 -0500

    Fleshed out study description
    Currently implements the title section of the citation

--- R/docroot.R
+++ R/docroot.R
@@ -7,21 +7,104 @@ ddi_codeBook <- function(...) {
   )
 
   components <- dots_to_xml_components(...)
+  content <- components$content
 
   allowed_children <- c(
     "dataDscr",
     "docDscr",
     "fileDscr",
     "otherMat",
-    "studyDscr"
+    "stdyDscr"
   )
 
   build_branch_node(
     "codeBook",
     allowed_children = allowed_children,
+    required_children = "stdyDscr",
     root = TRUE,
-    content = components$content,
+    content = content,
     attribs = schema
+  )
+}
+
+#' Study description
+#' 
+#' All DDI codebooks must have a study description which
+#' contains information about the study overall, including
+#' a citation to the work(s) referenced in the codebook. At least
+#' one citation must be present, capturing the whole study.
+#'
+#' @export
+ddi_stdyDscr <- function(...) {
+  allowed_children <- c(
+    "citation",
+    "studyAuthorization"
+  )
+
+  components <- dots_to_xml_components(...)
+  
+  build_branch_node(
+    "stdyDscr",
+    allowed_children = allowed_children,
+    required_children = "citation",
+    attribs = components$attribs,
+    content = components$content
+  )
+}
+
+ddi_citation <- function(...) {
+  allowed_children <- c(
+    "titlStmt",
+    "biblCit"
+  )
+
+  components <- dots_to_xml_components(...)
+
+  build_branch_node(
+    "citation",
+    allowed_children = allowed_children,
+    required_children = "titlStmt",
+    components = components
+  )
+}
+
+ddi_titlStmt <- function(...) {
+  allowed_children <- c(
+    "titl",
+    "subTitl",
+    "altTitl",
+    "parTitl"
+  )
+
+  components <- dots_to_xml_components(...)
+
+  build_branch_node(
+    "titlStmt",
+    allowed_children = allowed_children,
+    components = components
+  )
+}
+
+ddi_titl <- simple_leaf_node("titl")
+ddi_subTitl <- simple_leaf_node("subTitl")
+ddi_altTitl <- simple_leaf_node("altTitl")
+
+ddi_parTitl <- function(language, ...) {
+  stopifnot(is.character(language))
+
+  components <- dots_to_xml_components(...)
+  attribs <- components$attribs
+
+  if (is.null(attribs)) {
+    attribs <- list()
+  }
+
+  attribs[["xml:lang"]] <- language
+
+  build_leaf_node(
+    "parTitl",
+    attribs = attribs,
+    content = components$content
   )
 }
 
@@ -39,6 +122,6 @@ ddi_dataDscr <- function(...) {
   build_branch_node(
     "dataDscr",
     allowed_children = allowed_children,
-    content = components$content
+    components = components
   )
 }
