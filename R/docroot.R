3c8e12880872b36d730c139012560440a1ceb436
dswoulf <78369907+dswoulf@users.noreply.github.com>
2021-04-15 16:39:16 -0400

    Update docroot.R

--- R/docroot.R
+++ R/docroot.R
@@ -1,14 +1,35 @@
+#' Codebook
+#' 
+#' The root node of a DDI 2.5 Codebook file. This file must contain stdyDscr
+#' 
+#' @param ... Child nodes or attributes
+#'
+#' @section DDI Codebook 2.5 Documentation:
+#'
+#' \url{https://ddialliance.org/Specification/DDI-Codebook/2.5/XMLSchema/field_level_documentation_files/schemas/codebook_xsd/elements/codeBook.html}
+#' 
+#' @export
 ddi_codeBook <- function(...) {
-  schema <- list(
-    version = "2.5",
-    xmlns = "ddi:codebook:2_5",
-    `xmlns:xsi` = "http://www.w3.org/2001/XMLSchema-instance",
-    `xsi:schemaLocation` = "http://www.ddialliance.org/Specification/DDI-Codebook/2.5/XMLSchema/codebook.xsd"
-  )
+#  schema <- list(
+#    version = "2.5",
+#    xmlns = "ddi:codebook:2_5",
+#    `xmlns:xsi` = "http://www.w3.org/2001/XMLSchema-instance",
+#    `xsi:schemaLocation` = "http://www.ddialliance.org/Specification/DDI-Codebook/2.5/XMLSchema/codebook.xsd"
+#  )
+# NEED TO ASK ABOUT ABOVE
 
   components <- dots_to_xml_components(...)
   content <- components$content
+  attribs <- components$attribs
 
+  if(!is.null(attribs)) {
+    allowed_attribs <- c("ID", "xml:lang", "source", "elementVersion", "elementVersionDate", "ddiLifecycleUrn", "ddiCodebookUrn",
+                        "version", "codeBookAgency")
+    if(!is.null(attribs$version) & attribs$version != "2.5") attribs$version = "2.5"
+    check_attribs_in_set(names(attribs), allowed_attribs, field = "codebook")
+    check_attribs(attribs)
+  }
+  
   allowed_children <- c(
     "dataDscr",
     "docDscr",
@@ -23,7 +44,7 @@ ddi_codeBook <- function(...) {
     required_children = "stdyDscr",
     root = TRUE,
     content = content,
-    attribs = schema
+    attribs = attribs
   )
 }
 
@@ -36,100 +57,50 @@ ddi_codeBook <- function(...) {
 #'
 #' @param ... Child nodes or attributes. To set a DDI ID, use `id_object` in any `ddi_` function to assign the identifier.
 #'
+#' @section DDI Codebook 2.5 Documentation:
+#' url{https://ddialliance.org/Specification/DDI-Codebook/2.5/XMLSchema/field_level_documentation_files/schemas/codebook_xsd/elements/stdyDscr.html}
+#' 
 #' @export
 ddi_stdyDscr <- function(...) {
   allowed_children <- c(
     "citation",
-    "studyAuthorization"
+    "studyAuthorization",
+    "stdyInfo",
+    "studyDevelopment",
+    "method",
+    "dataAccs",
+    "othrStdyMat",
+    "notes"
   )
 
   components <- dots_to_xml_components(...)
+  attribs <- components$attribs
   
+  if(!is.null(attribs)) {
+    allowed_attribs <- c("ID", "xml:lang", "source", "elementVersion", "elementVersionDate", "ddiLifecycleUrn", "ddiCodebookUrn",
+                        "access")
+    check_attribs_in_set(names(attribs), allowed_attribs, field = "stdyDscr")
+    check_attribs(attribs)
+  }
+
   build_branch_node(
     "stdyDscr",
     allowed_children = allowed_children,
     required_children = "citation",
-    attribs = components$attribs,
-    content = components$content
-  )
-}
-
-#' Create a DDI citation
-#'
-#' Citation entities belong in study and DDI document description blocks.
-#'
-#' @param ... Child nodes or attributes. To set a DDI ID, use `id_object` in any `ddi_` function to assign the identifier.
-#' @param language An xml:lang identifier representing the language of the parallel title (`ddi_parTitl()`)
-#' 
-#' @export
-ddi_citation <- function(...) {
-  allowed_children <- c(
-    "titlStmt",
-    "biblCit"
-  )
-
-  components <- dots_to_xml_components(...)
-
-  build_branch_node(
-    "citation",
-    allowed_children = allowed_children,
-    required_children = "titlStmt",
-    components = components
-  )
-}
-
-#' @rdname ddi_citation
-#' @export
-ddi_titlStmt <- function(...) {
-  allowed_children <- c(
-    "titl",
-    "subTitl",
-    "altTitl",
-    "parTitl"
-  )
-
-  components <- dots_to_xml_components(...)
-
-  build_branch_node(
-    "titlStmt",
-    allowed_children = allowed_children,
-    components = components
-  )
-}
-
-#' @rdname ddi_citation
-#' @export
-ddi_titl <- simple_leaf_node("titl")
-
-#' @rdname ddi_citation
-#' @export
-ddi_subTitl <- simple_leaf_node("subTitl")
-
-#' @rdname ddi_citation
-#' @export
-ddi_altTitl <- simple_leaf_node("altTitl")
-
-#' @rdname ddi_citation
-#' @export
-ddi_parTitl <- function(language, ...) {
-  stopifnot(is.character(language))
-
-  components <- dots_to_xml_components(...)
-  attribs <- components$attribs
-
-  if (is.null(attribs)) {
-    attribs <- list()
-  }
-
-  attribs[["xml:lang"]] <- language
-
-  build_leaf_node(
-    "parTitl",
     attribs = attribs,
     content = components$content
   )
 }
 
+#' Variable description
+#' 
+#' Description of variables within the Codebook. 
+#' @param ... Child nodes or attributes. 
+#'
+#' @section DDI Codebook 2.5 Documentation:
+#' url{https://ddialliance.org/Specification/DDI-Codebook/2.5/XMLSchema/field_level_documentation_files/schemas/codebook_xsd/elements/dataDscr.html}
+#' 
+#' @export
 ddi_dataDscr <- function(...) {
   allowed_children <- c(
     "nCube",
@@ -140,10 +111,20 @@ ddi_dataDscr <- function(...) {
   )
 
   components <- dots_to_xml_components(...)
+  attribs <- components$attribs
+  
+  if(!is.null(attribs)) {
+    allowed_attribs <- c("ID", "xml:lang", "source", "elementVersion", "elementVersionDate", "ddiLifecycleUrn", "ddiCodebookUrn")
+    check_attribs_in_set(names(attribs), allowed_attribs, field = "dataDscr")
+    check_attribs(attribs)
+  }
+
+  components <- dots_to_xml_components(...)
 
   build_branch_node(
     "dataDscr",
     allowed_children = allowed_children,
+    attribs = attribs,
     components = components
   )
 }
