749badf64d2788ea706f651c2ec3bac7d1e44fd1
dswoulf <78369907+dswoulf@users.noreply.github.com>
2021-04-30 12:02:45 -0400

    Update test-variables.R

--- tests/testthat/test-variables.R
+++ tests/testthat/test-variables.R
@@ -1,1 +1,10 @@
 context("DDI Variable specification")
+
+test_that("Attribute is in set", {
+    expect_equal(check_attribs_in_set("one", c("a", "one", "orange"), "test"), "one")
+})
+
+test_that("Element cardinality", {
+    test_node <- ddi_var("test", ddi_notes("one"), ddi_notes("two"), ddi_notes("three"))
+    expect_equal(check_cardinality(test_node$content, "notes"), 3)
+})
